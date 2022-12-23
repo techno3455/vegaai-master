cce4647d3648bb2b1a2b01cf5c2bded6f2868b61
Trevor Blanchard <72676508+techno3455@users.noreply.github.com>
2022-12-23 08:24:29 -0500

    Add files via upload

---
+++ veganetwork/veganetworkmain/veganetworkmain/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def home_view(request):
+        user = request.user
+        hello = 'Hello world'
+        context = {'user':user, 'hello':hello}
+        return render(request, 'main/signin.html', context)
